4b987bf643d38acdad6e5b2ca8e0629b23d97e89
rafal-szymanko <61496255+rafal-szymanko@users.noreply.github.com>
2020-08-20 09:18:20 +0200

    Add mongoose models for department&employees collections

---
+++ models/employees.model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const employeesSchema = new mongoose.Schema({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    department: { type: String, required: true },
+  });
+
+
+  module.exports = mongoose.model('Employee', employeesSchema);
